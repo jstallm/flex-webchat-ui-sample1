73f3270bf8710da98967fbc3a6623ed6d9d9a237
Joey Stallmeyer <jstallmeyer@sureco.com>
2019-09-10 11:05:05 -0700

    add template

---
+++ src/flex-webchat-ui-sample1/Controllers/HomeController.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using flex_webchat_ui_sample1.Models;
+
+namespace flex_webchat_ui_sample1.Controllers
+{
+    public class HomeController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult Privacy()
+        {
+            return View();
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
---
+++ src/flex-webchat-ui-sample1/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace flex_webchat_ui_sample1.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
